ef776737eb9c16816574a9b186058b1893f82016
ILuvLitten <111524635+ILuvLitten@users.noreply.github.com>
2024-04-03 13:40:07 -0500

    Update Button.java

--- Button.java
+++ Button.java
@@ -15,6 +15,7 @@ public class Button{
   private boolean hover = false;
   private boolean disabled = false;
   private boolean hidden = false;
+  private int r = 0;
 
   //Rectangular Button
   public Button(int x, int y, int w, int h){
@@ -27,8 +28,8 @@ public class Button{
     int[] xPoints = new int[4];
     xPoints[0] = x;
     xPoints[1] = x+w;
-    xPoints[2] = x;
-    xPoints[3] = x+w;
+    xPoints[2] = x+w;
+    xPoints[3] = x;
 
     int[] yPoints = new int[4];
     yPoints[0] = y;
@@ -42,6 +43,7 @@ public class Button{
   //Hexagonal Button
   public Button(int x, int y, int d){
     int radius = d/2;
+    r = radius;
     xPos = x;
     yPos = y;
     int[] xPoints = new int[6];
@@ -57,13 +59,20 @@ public class Button{
     xPoints[3] = x;
     xPoints[4] = x-h;
     xPoints[5] = x-h;
+    for(int i = 0; i < 6; i++) {
+    	xPoints[i]=xPoints[i]+radius;
+    }
 
-    yPoints[0] = y + r;
-    yPoints[1] = y + r/2;
-    yPoints[2] = y - r/2;
-    yPoints[3] = y - r;
-    yPoints[4] = y - r/2;
-    yPoints[5] = y + r/2;
+    yPoints[0] = y + radius;
+    yPoints[1] = y + radius/2;
+    yPoints[2] = y - radius/2;
+    yPoints[3] = y - radius;
+    yPoints[4] = y - radius/2;
+    yPoints[5] = y + radius/2;
+    for(int i = 0; i < 6; i++) {
+    	yPoints[i]=yPoints[i]+radius;
+    }
+    
 
     polygon = new Polygon(xPoints, yPoints, 6);
   }
@@ -85,6 +94,30 @@ public class Button{
       g.drawString(text, xPos+(width/2),yPos+(height/2));
     }
     
+  }
+  
+  public int[] transform(int[] points, int newMax) {
+	  int[] newPoints = new int[points.length];
+	  int start = Integer.MAX_VALUE;
+	  for(int i = 0; i < points.length; i++) {
+		  if(points[i] < start) {
+			  start = points[i];
+		  }
+	  }
+	  int max = Integer.MIN_VALUE;
+	  for(int i = 0; i < points.length; i++) {
+		  points[i] = points[i] - start;
+		  if(points[i] > max) {
+			  max = points[i];
+		  }
+	  }
+	  for(int i = 0; i < points.length; i++) {
+		  double v = ((double)points[i])/max;
+		  newPoints[i] = (int) v*newMax;
+	  }
+	  
+	  
+	  return newPoints;
   }
 
   public boolean mouseMoved(int clickX, int clickY){
