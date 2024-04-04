03acd67937341226cfd99aa8f676c0c4d1307606
alonsoramos1 <147020148+alonsoramos1@users.noreply.github.com>
2024-04-04 02:14:35 -0500

    Update CenterBoard.java

--- CenterBoard.java
+++ CenterBoard.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
 public class CenterBoard {
+    private ArrayList<AnimalToken> tokenList;
+    private ArrayList<HabitatTile> tileList;
+    private boolean overpopulation3, overpopulation4;
+    private BagOfAnimalTokens bag;
+
+    public CenterBoard() {
+        tokenList = new ArrayList<AnimalToken>();
+        tileList = new ArrayList<HabitatTile>();
+        overpopulation3 = false;
+        overpopulation4 = false;
+        bag = new BagOfAnimalTokens();
+    }
+
+    public ArrayList<AnimalToken> getTokenList() {
+        return tokenList;
+    }
+
+    public ArrayList<HabitatTile> getTileList() {
+        return tileList;
+    }
+
 }
