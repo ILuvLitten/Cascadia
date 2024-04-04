9353bd6da8e4a7ec6f98ffc4a18130a97f9869df
alonsoramos1 <147020148+alonsoramos1@users.noreply.github.com>
2024-04-04 00:45:59 -0500

    Update to AnimalToken class

--- AnimalToken.java
+++ AnimalToken.java
@@ -1,2 +1,22 @@
+import java.awt.image.BufferedImage;
 public class AnimalToken {
+    private int animalType;
+    private BufferedImage image;
+
+    public AnimalToken(int type, BufferedImage img) {
+        animalType = type;
+        image = img;
+    }
+
+    public int getAnimalType() {
+        return animalType;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public void setImage(BufferedImage x) {
+        image = x;
+    }
 }
