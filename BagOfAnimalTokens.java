033b2a1bbd87d47ec6a45545a67bce954b33361f
alonsoramos1 <147020148+alonsoramos1@users.noreply.github.com>
2024-04-04 19:59:11 -0500

    Update BagOfAnimalTokens.java

--- BagOfAnimalTokens.java
+++ BagOfAnimalTokens.java
@@ -1,33 +1,67 @@
 import java.util.ArrayList;
 import java.util.Queue;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Collections;
+
 
 public class BagOfAnimalTokens {
-    private Queue<AnimalToken> tokenList;
+    private ArrayList<AnimalToken> tokenList;
     
     public BagOfAnimalTokens()
     {
-        tokenList = new LinkedList<AnimalToken>();
+        tokenList = new ArrayList<AnimalToken>();
+        // add 20 bears
+        for (int i=0; i<20; i++) {
+            tokenList.add(new AnimalToken(1));
+        }
+        // add 20 elk
+        for (int i=0; i<20; i++) {
+            tokenList.add(new AnimalToken(2));
+        }
+        // add 20 salmon
+        for (int i=0; i<20; i++) {
+            tokenList.add(new AnimalToken(3));
+        }
+        // add 20 hawks
+        for (int i=0; i<20; i++) {
+            tokenList.add(new AnimalToken(4));
+        }
+        // add 20 foxes
+        for (int i=0; i<20; i++) {
+            tokenList.add(new AnimalToken(5));
+        }
+        Collections.shuffle(tokenList);
+
     }
 
     public void addTokens(AnimalToken one, AnimalToken two, AnimalToken three) {
-        tokenList.offer(one);
-        tokenList.offer(two);
-        tokenList.offer(three);
+        tokenList.add(one);
+        tokenList.add(two);
+        tokenList.add(three);
+        Collections.shuffle(tokenList);
     }
 
     public void addTokens(AnimalToken one, AnimalToken two, AnimalToken three, AnimalToken four) {
-        tokenList.offer(one);
-        tokenList.offer(two);
-        tokenList.offer(three);
-        tokenList.offer(four);
+        tokenList.add(one);
+        tokenList.add(two);
+        tokenList.add(three);
+        tokenList.add(four);
+        Collections.shuffle(tokenList);
     }
 
     public ArrayList<AnimalToken> draw(int x) {
         ArrayList<AnimalToken> list = new ArrayList<AnimalToken>();
         for (int i=0; i<x; i++) {
-            list.add(tokenList.poll());
+            list.add(tokenList.remove(0));
         }
+        Collections.shuffle(tokenList);
         return list;
     }
+
+    public AnimalToken draw() {
+        AnimalToken t = tokenList.remove(0);
+        Collections.shuffle(tokenList);
+        return t;
+    }
 }
+
