741af3fa220676dbd8117849d8b4b7f761daf077
alonsoramos1 <147020148+alonsoramos1@users.noreply.github.com>
2024-04-04 20:51:41 -0500

    Update CenterBoard.java

--- CenterBoard.java
+++ CenterBoard.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.util.ArrayList;
 public class CenterBoard {
     private AnimalToken[] tokenList;
@@ -6,7 +7,7 @@ public class CenterBoard {
     private BagOfAnimalTokens bag;
     private TilePile stack;
 
-    public CenterBoard() {
+    public CenterBoard() throws IOException {
         tokenList = new AnimalToken[4];
         tileList = new HabitatTile[4];
         overpopulation3 = false;
@@ -50,38 +51,35 @@ public class CenterBoard {
 
     public HabitatTile drawTile1() {
         HabitatTile t1 = tileList[0];
-        tokenList[0] = null;
+        tileList[0] = null;
         return t1;
     }
 
     public HabitatTile drawTile2() {
         HabitatTile t2 = tileList[1];
-        tokenList[1] = null;
+        tileList[1] = null;
         return t2;
     }
 
     public HabitatTile drawTile3() {
         HabitatTile t3 = tileList[2];
-        tokenList[2] = null;
+        tileList[2] = null;
         return t3;
     }
 
     public HabitatTile drawTile4() {
         HabitatTile t4 = tileList[3];
-        tokenList[3] = null;
+        tileList[3] = null;
         return t4;
     }
 
-    // refills the animal token array with four new animal tokens
+    // replaces any missing tokens
     public void fillTokens() {
-        for (int i=0; i<4; i++)
-            tokenList[i] = bag.draw();
+        for (int i=0; i<4; i++) {
+            if (tokenList[i]==null)
+                tokenList[i] = bag.draw();
+        }
     }
-
-   // public void fillTiles() {
-   //     for (int i=0; i<4; i++)
-   //         tileList[i] = stack.draw();
-   // }
 
     public void checkOverpop() {
         int type1 = tokenList[0].getAnimalType();
@@ -92,6 +90,9 @@ public class CenterBoard {
         // checks if all 4 available animal token are of the same type; overpopulation
         if (type1 == type2 && type1 == type3 && type1 == type4)
             overpopulation4 = true;
+        else {
+            overpopulation4 = false;
+        }
 
         ArrayList<Integer> list = new ArrayList<Integer>();
         list.add(type1);
@@ -99,11 +100,51 @@ public class CenterBoard {
         list.add(type3);
         list.add(type4);
         
-        int x = 0;
-
+        // checks for overpop using token 1
+        boolean check1 = false;
+        int x1 = 0;
+        for (int a: list) {
+            if (a==type1)
+                x1++;
+        }
+        if (x1==3)
+            check1 = true;
         
+        // checks for overpop using token 2
+        boolean check2 = false;
+        int x2 = 0;
+        for (int b: list) {
+            if (b==type2)
+                x2++;
+        }
+        if (x2==3)
+            check2 = true;
+        
+        // checks for overpop using token 3
+        boolean check3 = false;
+        int x3 = 0;
+        for (int c: list) {
+            if (c==type3)
+                x3++;
+        }
+        if (x3==3)
+            check3 = true;
+        
+        // checks for overpop using token 4
+        boolean check4 = false;
+        int x4 = 0;
+        for (int d: list) {
+            if (d==type4)
+                x4++;
+        }
+        if (x4==3)
+            check4 = true;
+        
+        if (check1 || check2 || check3 || check4)
+            overpopulation3 = true;
+        else {
+            overpopulation3 = false;
+        }
     }
-
-    
-
 }
+
