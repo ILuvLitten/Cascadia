fa7457c1ef11b6dd37b7b3d59a70b315aae77de1
alonsoramos1 <147020148+alonsoramos1@users.noreply.github.com>
2024-04-16 13:27:58 -0500

    Update GameBoard.java

--- GameBoard.java
+++ GameBoard.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import static java.lang.System.*;
 public class GameBoard {
     private Hex[][] board;
     private int natureTokens;
@@ -11,6 +12,10 @@ public class GameBoard {
             }
         }
         natureTokens = 0;
+    }
+
+    public Hex[][] getBoard() {
+        return board;
     }
 
     public boolean placeTile(int r, int c, HabitatTile tile) {
@@ -26,8 +31,8 @@ public class GameBoard {
         int type = t.getAnimalType();
         HabitatTile tile = board[r][c].getTile();
 
-        if (type == 1) {
-            if (tile.getCanHoldBear()) {
+        if (type == 1 && tile!=null) {
+            if (!tile.getContainsAnimal() && tile.getCanHoldBear()) {
                 tile.setToken(t);
                 tile.setContainsAnimal(true);
                 if (tile.getIsKeystone())
@@ -36,9 +41,9 @@ public class GameBoard {
             }
         }
 
-        if (type == 2) {
+        if (type == 2 && tile!=null) {
 
-            if (tile.getCanHoldElk()) {
+            if (!tile.getContainsAnimal() && tile.getCanHoldElk()) {
                 tile.setToken(t);
                 tile.setContainsAnimal(true);
                 if (tile.getIsKeystone())
@@ -47,8 +52,8 @@ public class GameBoard {
             }
         }
 
-        if (type == 3) {
-            if (tile.getCanHoldSalmon()) {
+        if (type == 3 && tile!=null) {
+            if (!tile.getContainsAnimal() && tile.getCanHoldSalmon()) {
                 tile.setToken(t);
                 tile.setContainsAnimal(true);
                 if (tile.getIsKeystone())
@@ -57,8 +62,8 @@ public class GameBoard {
             }
         }
 
-        if (type == 4) {
-            if (tile.getCanHoldHawk()) {
+        if (type == 4 && tile!=null) {
+            if (!tile.getContainsAnimal() && tile.getCanHoldHawk()) {
                 tile.setToken(t);
                 tile.setContainsAnimal(true);
                 if (tile.getIsKeystone())
@@ -67,8 +72,8 @@ public class GameBoard {
             }
         }
 
-        if (type == 5) {
-            if (tile.getCanHoldFox()) {
+        if (type == 5 && tile!=null) {
+            if (!tile.getContainsAnimal() && tile.getCanHoldFox()) {
                 tile.setToken(t);
                 tile.setContainsAnimal(true);
                 if (tile.getIsKeystone())
@@ -80,12 +85,14 @@ public class GameBoard {
     }
 
     public Hex getAdjHex(int r, int c, int s) {
-        int[] evenCol = {-1, -1, 0, 1, 1, 0};
+        // r==2, c==1, s==1
+        int[] evenCol = {-1, 0, 1, 0, -1, -1};
         int[] evenRow = {-1, -1, 0, 1, 1, 0};
         int[] oddCol = {0, 1, 1, 1, 0, -1};
         int[] oddRow = {-1, -1, 0, 1, 1, 0};
         Hex h = board[r][c];
         boolean even = (r%2 == 0);
+
         if (r==0) {
             if (s==0 || s==1) {
                 return null;
@@ -122,6 +129,7 @@ public class GameBoard {
         }
         else {
             return board[r+evenRow[s]][c+evenCol[s]];
+            // return board[r-1][c-1]
         }
 
     }
@@ -140,7 +148,7 @@ public class GameBoard {
         ArrayList<Hex> list = getAdjacents(row, col);
 
         for (Hex h: list) {
-            if (h.getTile() != null) {
+            if (h!=null && h.getTile() != null) {
                 if (h.getTile().containsHawk())
                     return 0;
             }
@@ -183,7 +191,7 @@ public class GameBoard {
         int scoreFox =0;
         for (int r=0; r<42; r++) {
             for (int c=0; c<42; c++) {
-                if (board[r][c].getTile().containsFox()) {
+                if (!board[r][c].getEmpty() && board[r][c].getTile().containsFox()) {
                     scoreFox += uniqueFox(r, c);
                 }
             }
@@ -200,7 +208,7 @@ public class GameBoard {
         boolean adjacentFox = false;
         int total = 0;
         for (Hex h: list) {
-            if (h.getTile()!=null) {
+            if (h!=null && h.getTile()!=null) {
                 if (h.getTile().containsBear()) {
                     adjacentBear = true;
                 }
@@ -231,6 +239,156 @@ public class GameBoard {
         return total;
     }
 
+    public int calculateSalmon() {
+        int score = 0;
+        for (int r=0; r<42; r++) {
+            //out.println(r + " is " + board[5][1].getTile().getToken().getScored());
+            for (int c=0; c<42; c++) {
+                //if (r<5)
+                    //out.println(r + " " + c  + " " + board[5][1].getTile().getToken().getScored());
+                Hex h = board[r][c];
+                if (r == 5 && c == 1) {
+                   // out.println("5 1 " + h.getTile().getToken().getScored());
+                }
+                if (!h.getEmpty()) {
+                    out.println(r + " " + c + " not empty");
+                    if (h.getTile().containsSalmon()) {
+                        out.println(r + " " + c + " contains salmon");
+                        if (!h.getTile().getToken().getScored()) {
+                            out.println(r + " " + c + " not scored");
+                        }
+                    }
+                }
+                if (!h.getEmpty() && h.getTile().containsSalmon() && !h.getTile().getToken().getScored()) {
+                    int numAdjSalmon = 0;
+                    int side = 0;
+                    ArrayList<Hex> adjacentSalmons = new ArrayList<Hex>();
+                    //out.println(r + " " + c);
+                    for (int i=0; i<6; i++) {
+                        Hex spot = getAdjHex(r, c, i);
+                        if (spot!=null && !spot.getEmpty()) {
+                            if (spot.getTile().containsSalmon() && !spot.getTile().getToken().getScored()) {
+                                numAdjSalmon++;
+                                side = i;
+                                adjacentSalmons.add(spot);
+                            }
+                        }
+                    }
+                    if (adjacentSalmons.isEmpty()) {
+                        score += 2;
+                        board[r][c].getTile().getToken().setScored(true);
+                    }
+                    if (numAdjSalmon==1) {
+                        boolean b = adjacentRun(r, c, side);
+                        if (b) {
+                            int size = runSize(r, c) + 1;
+                            out.println("size " + size);
+                            if (size < 3) {
+                                score += 5;
+                            } else if (size < 4) {
+                                score += 8;
+                            } else if (size < 5) {
+                                score += 12;
+                            } else if (size < 6) {
+                                score += 16;
+                            } else if (size < 7) {
+                                score += 20;
+                            } else {
+                                score += 25;
+                            }
+                        }
+                    }
+                    if (numAdjSalmon == 2) {
+                        Hex firstSalmon = adjacentSalmons.get(0);
+                        //out.println("adjacentone " + r + " " + c + " row and column: " + firstSalmon.getRow() + " " + firstSalmon.getColumn());
+                        Hex secondSalmon = adjacentSalmons.get(1);
+                        //out.println("adjacenttwo " + r + " " + c + " row and column: " + secondSalmon.getRow() + " " + secondSalmon.getColumn());
+                        if (findTypeAdjacent(firstSalmon)<=2 && findTypeAdjacent(secondSalmon)<=2) {
+                            score += 8;
+                            board[r][c].getTile().getToken().setScored(true);
+                            firstSalmon.getTile().getToken().setScored(true);
+                            secondSalmon.getTile().getToken().setScored(true);
+                        }
+                    }
+                }
+            }
+        }
+        return score;
+
+    }
+
+    public int findTypeAdjacent(Hex h) {
+        int cnt = 0;
+        if (!h.getEmpty() && h.getTile().getContainsAnimal()) {
+            ArrayList<Hex> adjacents = getAdjacents(h.getRow(), h.getColumn());
+            for (Hex spot: adjacents) {
+                if (spot != null && !spot.getEmpty() && spot.getTile().getContainsAnimal() && spot.getTile().getToken().getAnimalType() == h.getTile().getToken().getAnimalType()) {
+                    cnt++;
+                }
+                
+            }
+        }
+        return cnt;
+    }
+
+    public int runSize(int r, int c) {
+        int side = 0;
+        ArrayList<Hex> adjacentSalmons = new ArrayList<Hex>();
+        board[r][c].getTile().getToken().setScored(true);
+        for (int i=0; i<6; i++) {
+                Hex spot = getAdjHex(r, c, i);
+                if (spot!=null && !spot.getEmpty()) {
+                    if (spot.getTile().containsSalmon() && !spot.getTile().getToken().getScored()) {
+                        //numAdjSalmon++;
+                        side = i;
+                        adjacentSalmons.add(spot);
+                    }
+                }
+
+        }
+        if (adjacentSalmons.isEmpty()) {
+            return 0;
+        } else {
+            Hex hexagon = adjacentSalmons.get(0);
+            return 1 + runSize(hexagon.getRow(), hexagon.getColumn());
+        }
+    }
+
+    public boolean adjacentRun(int r, int c, int s) {
+        Hex hexagon = board[r][c];
+        //int numAdjSalmon = 0;
+        int side = 0;
+        int blockedSide = s + 3;
+        if (blockedSide == 6) {
+            blockedSide = 0;
+        }
+        ArrayList<Hex> adjacentSalmons = new ArrayList<Hex>();
+        for (int i=0; i<6; i++) {
+            if (i!=blockedSide) {
+                Hex spot = getAdjHex(r, c, i);
+                if (spot!=null && !spot.getEmpty()) {
+                    if (spot.getTile().containsSalmon() && !spot.getTile().getToken().getScored()) {
+                        //numAdjSalmon++;
+                        side = i;
+                        adjacentSalmons.add(spot);
+                    }
+                }
+            }
+        }
+        int cnt = adjacentSalmons.size();
+        if (cnt==1 || cnt==0) {
+            if (cnt == 1) {
+                Hex spot1 = adjacentSalmons.get(0);
+                boolean one = adjacentRun(spot1.getRow(), spot1.getColumn(), side);
+                return one;
+            }
+            if (cnt == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public int calculateBear() {
         int numPairs = 0;
         for (int r=0; r<42; r++) {
@@ -240,7 +398,7 @@ public class GameBoard {
                     boolean pair = pairBear(r, c);
                     if (pair) {
                         board[r][c].getTile().getToken().setScored(true);
-                        numPairs = numPairs + 1;
+                        numPairs++;
                     }
                 }
             }
@@ -262,12 +420,21 @@ public class GameBoard {
         ArrayList<Hex> adjacents = getAdjacents(row, col);
         int numAdjacentBears = 0;
         ArrayList<Hex> matches = new ArrayList<Hex>();
+        if (row==2 && col==1) {
+            //out.println("calculating " + row + " " + col);
+        }
         for (Hex h: adjacents) {
-            if (!h.getEmpty()) {
+            if (h!=null && !h.getEmpty()) {
                 if (h.getTile().containsBear()) {
+                    if (row==2 && col==1) {
+                        //out.println(h.getRow() + " " + h.getColumn() + "contains bear");
+                        //out.println("done");
+                    }
+                    //out.println(h.getTile().getToken().getAnimalType());
                     if (!h.getTile().getToken().getScored()) {
                         numAdjacentBears++;
                         matches.add(h);
+                        //out.println(row + " " + col + "passed");
                     }
                 }
             }
@@ -277,7 +444,7 @@ public class GameBoard {
             ArrayList<Hex> matchAdjacents = getAdjacents(match.getRow(), match.getColumn());
             int numAdjacentToMatch = 0;
             for (Hex h: matchAdjacents) {
-                if (!h.getEmpty()) {
+                if (h!=null && !h.getEmpty()) {
                     if (h.getTile().containsBear()) {
                         numAdjacentToMatch = numAdjacentToMatch + 1;
                     }
@@ -292,23 +459,7 @@ public class GameBoard {
         return false;
     }
 
-    //public int calculateElk(int row, int col) {
-    //    int scoreElk = 0;
-    //    Hex h = board[row][col];
-    //    ArrayList<Hex> adjacents = getAdjacents(row, col);
-//
-    //    for (int s=2; s<6; s++) {
-    //        Hex adj = adjacents.get(s);
-    //        if( adj.getTile().containsElk()){
-    //            return 1 + calculateElk(adj.getRow(), adj.getColumn());
-    //       }
-     //   }
 
-
-
-    //} 
-
-    
 
     public int returnNatureTokens() {
         return natureTokens;
