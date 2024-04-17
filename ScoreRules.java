7b8d872ff8cfbf9fc5732efa29b70bad01b40ed1
Dhairya-Gama <dvgama07@gmail.com>
2024-04-17 12:59:49 -0500

    uwu

---
+++ ScoreRules.java
@@ -0,0 +1,39 @@
+import java.awt.image.BufferedImage;
+
+public class ScoreRules {
+
+    private BufferedImage bearRule;
+    private BufferedImage salmonRule;
+    private BufferedImage hawkRule;
+    private BufferedImage foxRule;
+    private BufferedImage elkRule;
+
+    private int bearRule;
+    private int salmonRule;
+    private int hawkRule;
+    private int foxRule;
+    private int elkRule;
+
+
+
+
+    public ScoreRules(int bear, int salmon, int hawk, int fox, int elk)
+    {
+        bearRule = bear;
+        salmonRule = salmon;
+        hawkRule = hawk;
+        foxRule = fox;
+        elkRule = elk;
+    }
+
+    public getBearRule()
+    {
+
+    }
+
+
+
+
+
+
+}
