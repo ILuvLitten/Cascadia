5962d17a9fbb15faeed2c23bc89923772139528b
alonsoramos1 <147020148+alonsoramos1@users.noreply.github.com>
2024-04-18 13:28:50 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,2 +1,22 @@
 public class Player {
+    private GameBoard board;
+    private Score scoreCard;
+
+    public Player() {
+        board = new GameBoard();
+        scoreCard = new Score(board);
+    }
+
+    public GameBoard getBoard() {
+        return board;
+    }
+
+    public int getNatureTokens() {
+        return board.getNatureTokens();
+    }
+  
+    public Score getScoreCard() {
+        return scoreCard
+
+    
 }
