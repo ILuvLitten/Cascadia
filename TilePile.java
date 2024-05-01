fb298710932589fd8c98ab870d5130d5a913489e
Dhairya-Gama <dvgama07@gmail.com>
2024-05-01 13:29:16 -0500

    updates on score rules

--- TilePile.java
+++ TilePile.java
@@ -12,39 +12,57 @@ public class TilePile {
     private ArrayList<HabitatTile> pile;
     private ArrayList<StarterTile> stPile;
     Scanner kb = new Scanner(new File("Text File for Images Cascadia.txt"));
+    BufferedImage topImg1 = ImageIO.read(TilePile.class.getResource("/images/1-01000-01000-01000.png"));
+    BufferedImage leftImg1 = ImageIO.read(TilePile.class.getResource("/images/0-10000-00001-11010.png"));
+    BufferedImage rightImg1 = ImageIO.read(TilePile.class.getResource("/images/0-00100-00010-00101.png"));
 
+    BufferedImage topImg2 = ImageIO.read(TilePile.class.getResource("/images/1-00100-00100-00001.png"));
+    BufferedImage leftImg2 = ImageIO.read(TilePile.class.getResource("/images/0-00010-10000-00111.png"));
+    BufferedImage rightImg2 = ImageIO.read(TilePile.class.getResource("/images/0-10000-01000-11000.png"));
+
+    BufferedImage topImg3 = ImageIO.read(TilePile.class.getResource("/images/1-00010-00010-00010.png"));
+    BufferedImage leftImg3 = ImageIO.read(TilePile.class.getResource("/images/0-00001-01000-01110.png"));
+    BufferedImage rightImg3 = ImageIO.read(TilePile.class.getResource("/images/0-10000-00100-10001.png"));
+
+    BufferedImage topImg4 = ImageIO.read(TilePile.class.getResource("/images/1-10000-10000-10000.png"));
+    BufferedImage leftImg4 = ImageIO.read(TilePile.class.getResource("/images/0-01000-00010-01011.png"));
+    BufferedImage rightImg4 = ImageIO.read(TilePile.class.getResource("/images/0-00100-00001-10100.png"));
+
+    BufferedImage topImg5 = ImageIO.read(TilePile.class.getResource("/images/1-00001-00001-00100.png"));
+    BufferedImage leftImg5 = ImageIO.read(TilePile.class.getResource("/images/0-00100-01000-11100.png"));
+    BufferedImage rightImg5 = ImageIO.read(TilePile.class.getResource("/images/0-00010-10000-00011.png"));
 
     public TilePile() throws IOException {
         pile = new ArrayList<HabitatTile>();
         stPile = new ArrayList<StarterTile>();
         // add image in last parameter
-        HabitatTile top1 = new HabitatTile(true, 2, 2, false, true, false, false, false, IMAGE);
-        HabitatTile left1 = new HabitatTile(false, 1, 5, true, true, false, true, false, IMAGE);
-        HabitatTile right1 = new HabitatTile(false, 3, 4, false, false, true, false, true, IMAGE);
+        HabitatTile top1 = new HabitatTile(true, 2, 2, false, true, false, false, false, topImg1);
+        HabitatTile left1 = new HabitatTile(false, 1, 5, true, true, false, true, false, leftImg1);
+        HabitatTile right1 = new HabitatTile(false, 3, 4, false, false, true, false, true, rightImg1);
         StarterTile starter1 = new StarterTile(top1, left1, right1);
         stPile.add(starter1);
 
-        HabitatTile top2 = new HabitatTile(true, 3, 3, false, false, false, false, true, IMAGE);
-        HabitatTile left2 = new HabitatTile(false, 4, 5, false, false, true, true, true, IMAGE);
-        HabitatTile right2 = new HabitatTile(false, 1, 2, true, true, false, false, false. IMAGE);
+        HabitatTile top2 = new HabitatTile(true, 3, 3, false, false, false, false, true, topImg2);
+        HabitatTile left2 = new HabitatTile(false, 4, 5, false, false, true, true, true, leftImg2);
+        HabitatTile right2 = new HabitatTile(false, 1, 2, true, true, false, false, false, rightImg2);
         StarterTile starter2 = new StarterTile(top2, left2, right2);
         stPile.add(starter2);
 
-        HabitatTile top3 = new HabitatTile(true, 4, 4, false, false, false, true, false, IMAGE);
-        HabitatTile left3 = new HabitatTile(false, 5, 2, false, true, true, true, false, IMAGE);
-        HabitatTile right3 = new HabitatTile(false, 1, 3, true, false, false, false, true, IMAGE);
+        HabitatTile top3 = new HabitatTile(true, 4, 4, false, false, false, true, false, topImg3);
+        HabitatTile left3 = new HabitatTile(false, 5, 2, false, true, true, true, false, leftImg3);
+        HabitatTile right3 = new HabitatTile(false, 1, 3, true, false, false, false, true, rightImg3);
         StarterTile starter3 = new StarterTile(top3, left3, right3);
         stPile.add(starter3);
 
-        HabitatTile top4 = new HabitatTile(true, 1, 1, true, false, false, false, false, IMAGE);
-        HabitatTile left4 = new HabitatTile(false, 2, 4, false, true, false, true, true, IMAGE);
-        HabitatTile right4 = new HabitatTile(false, 3, 5, true, false, true, false, false, IMAGE);
+        HabitatTile top4 = new HabitatTile(true, 1, 1, true, false, false, false, false, topImg4);
+        HabitatTile left4 = new HabitatTile(false, 2, 4, false, true, false, true, true, leftImg4);
+        HabitatTile right4 = new HabitatTile(false, 3, 5, true, false, true, false, false, rightImg4);
         StarterTile starter4 = new StarterTile(top4, left4, right4);
         stPile.add(starter4);
 
-        HabitatTile top5 = new HabitatTile(true, 5, 5, false, false, true, false, false, IMAGE);
-        HabitatTile left5 = new HabitatTile(false, 3, 2, true, true, true, false, false, IMAGE);
-        HabitatTile right5 = new HabitatTile(false, 4, 1, false, false, false, true, true, IMAGE);
+        HabitatTile top5 = new HabitatTile(true, 5, 5, false, false, true, false, false, topImg5);
+        HabitatTile left5 = new HabitatTile(false, 3, 2, true, true, true, false, false, leftImg5);
+        HabitatTile right5 = new HabitatTile(false, 4, 1, false, false, false, true, true, rightImg5);
         StarterTile starter5 = new StarterTile(top5, left5, right5);
         stPile.add(starter5);
 
